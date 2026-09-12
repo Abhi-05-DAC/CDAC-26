e739cb1146a055bbed52ad597bfadc04eea88c98
Abhi-05-DAC <abhishek.wadile.cmaug26@gmail.com>
2026-09-12 16:43:32 +0530

    Add files via upload

---
+++ Assignments/OOPJ/assignment2/Program.java
@@ -0,0 +1,168 @@
+package assignment2;
+
+//public class Program				//Loops Exercise 1st Program
+//{
+//	public static void main(String[]args)
+//	{
+//		for(int i = 0; i < 1000; i++)
+//		{
+//			if(i % 2 != 0)
+//			{
+//				System.out.println(i);
+//			}
+//		}
+//	}
+//}
+
+
+
+//public class Program				//Second Program
+//{
+//	public static void main(String[]args)
+//	{
+//		for(int i = 0; i <= 500; i++)
+//		{
+//			if(i % 2 == 0)
+//			{
+//				System.out.println(i);
+//			}
+//		}
+//	}
+//}
+
+
+
+//public class Program					//Third Program
+//{
+//	public static void main(String[]args)
+//	{
+//		for(int i = 0; i <= 200; i++)
+//		{
+//			if(i % 7 == 0)
+//			{
+//				System.out.println(i);
+//			}
+//		}
+//	}
+//}
+
+
+
+//public class Program		
+//{ 
+//	public static void main(String[]args)						//Fourth Program   
+//	{
+//		for(int i = 1; i <= 5; i++)
+//		{
+//			for(int j = 1; j <= i; j++)
+//			{
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+//	}
+//}
+
+
+
+
+//public class Program		
+//{ 
+//	public static void main(String[]args)						//Fifth Program   
+//	{
+//		for(int i = 5; i >= 1; i--)
+//		{
+//			for(int j = 1; j <= i; j++)
+//			{
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+//	}
+//}
+
+
+
+
+
+//public class Program		
+//{ 
+//	public static void main(String[]args)						//Sixth Program   
+//	{
+//		int Num = 1;
+//		for(int i = 1; i <= 4; i++)
+//		{
+//			for(int j = 1; j <= i; j++)
+//			{
+//				System.out.print(Num + " ");
+//				Num ++;
+//			}
+//			System.out.println();
+//		}
+//	}
+//}
+
+
+
+
+//public class Program		
+//{ 
+//	public static void main(String[]args)						//Seventh Program   
+//	{
+//		for(int i = 6; i >= 1; i--)
+//		{
+//			for(int j = 1 ; j <= i; j++)
+//			{
+//				System.out.print(j );
+//			}
+//			System.out.println();
+//		}
+//	}
+//}
+
+
+
+
+
+//public class Program		
+//{ 
+//	public static void main(String[]args)						//Eighth Program   
+//	{
+//		for(int i = 1; i <= 6; i++)
+//		{
+//			for(int j = 1 ; j <= i; j++)
+//			{
+//				System.out.print(j);
+//			}
+//			System.out.println();
+//		}
+//	}
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
